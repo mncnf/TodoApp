3b53c38dec3cf0e74eaa06b05262aa0a25278ce9
mncnf <mncnf0113@gmail.com>
2025-05-19 21:24:23 +0900

    69

---
+++ TodoApp/UserData.swift
@@ -0,0 +1,16 @@
+//
+//  UserData.swift
+//  TodoApp
+//
+//  Created by Development on 2025/05/19.
+//
+
+import SwiftUI
+
+struct UserData {
+    var tasks = [
+        Task(title: "散歩", checked: true),
+        Task(title: "料理", checked: true),
+        Task(title: "筋トレ", checked: true)
+    ]
+}
