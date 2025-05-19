33806dd2f258b8bd30a2b07f6c15f33ac2f18793
mncnf <mncnf0113@gmail.com>
2025-05-19 12:38:37 +0900

    67

--- TodoApp/ListRow.swift
+++ TodoApp/ListRow.swift
@@ -9,15 +9,21 @@ import SwiftUI
 
 struct ListRow: View {
     let task: String
+    var isCheck: Bool
 
     var body: some View {
         HStack {
-            Text("◻︎")
+            if isCheck {
+                Text("☑︎")
+            } else {
+                Text("◻︎")
+            }
+
             Text(task)
         }
     }
 }
 
 #Preview {
-    ListRow(task: "料理")
+    ListRow(task: "料理", isCheck: true)
 }
